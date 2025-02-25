93b5957a839c2a12d5050a7acdf07c8f83e76de8
Sergio Mayombo Serrano <smayoser@myuax.com>
2025-02-25 13:51:01 +0100

    Commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import aplicacion.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        // You can pass arguments to Principal.main if needed
+        String[] principalArgs = {"1000000"}; // Example argument
+        Principal.main(principalArgs);
+    }
+}
---
+++ src/main/java/mates/Matematicas.java
@@ -0,0 +1,23 @@
+package mates;
+
+import java.util.Random;
+
+public class Matematicas {
+    /**
+     * Genera una aproximación al número pi mediante el metodo de Montecarlo.
+     *El parámetro puntosTotales muestra el  número de puntos a generar
+     */
+    public static double generarNumeroPi(long puntosTotales) {
+        if (puntosTotales <= 0) throw new IllegalArgumentException("El número de puntos debe ser mayor que cero");
+
+        Random random = new Random();
+        long aciertos = 0;
+
+        for (long i = 0; i < puntosTotales; i++) {
+            double x = random.nextDouble() * 2 - 1, y = random.nextDouble() * 2 - 1;
+            if (x * x + y * y <= 1) aciertos++;
+        }
+
+        return 4.0 * aciertos / puntosTotales;
+    }
+}
